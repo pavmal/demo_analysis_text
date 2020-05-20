1de3beced082381431ef6e3fd97e6f71a092f62a
Pavel Malinkin <pavmalin@mail.ru>
2020-05-20 23:17:21 +0300

    add phone reviews form analysis

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ print("Preparing classifier")
 start_time = time.time()
 spam_classifier = models.SentimentClassifier()
 tone_classifier = models.ToneSentimentClassifier()
-print("Classifier is ready")
+phone_review_classifier = models.PhoneReviewsToneSentimentClassifier()
+print("Classifiers are ready")
 print(time.time() - start_time, "seconds")
 
 
@@ -23,6 +24,7 @@ class DemoModel(FlaskForm):
     """
     analis_text = TextAreaField('Текст для анализа на спам', validators=[Length(min=0, max=250)])
     analis_text_tone = TextAreaField('Текст для анализа тональности', validators=[Length(min=0, max=250)])
+    phone_tone = TextAreaField('Текст для анализа тональности', validators=[Length(min=0, max=250)])
     submit = SubmitField('Отправить на обработку')
 
 
@@ -70,9 +72,28 @@ def render_main():
         else:
             prediction_message_tone = None
 
-        return render_template('demo.html', form=form, result=prediction_message, result_tone=prediction_message_tone)
+        # for phone reviews analysis
+        user_text_phone = form.phone_tone.data
+        if user_text_phone != '':
+            predictions_phone = phone_review_classifier.get_prediction_message(user_text_phone).split(',')
+            if predictions_phone[1] == 'positive':  # class 1 - positive feedback
+                class_res_phone = 'POSITIVE FEEDBACK'
+                persent_phone = predictions_phone[2]
+            elif predictions_phone[1] == 'negative':
+                class_res_phone = 'NEGATIVE FEEDBACK'
+                persent_phone = str(100 - float(predictions_phone[2]))
+            else:
+                class_res_phone = 'NEUTRAL'
+                persent_phone = predictions_phone[2]
+            prediction_message_phone = 'Ваш отзыв классифицирован как: {} c вероятностью: {} %'.format(class_res_phone,
+                                                                                                       persent_phone)
+        else:
+            prediction_message_phone = None
+
+        return render_template('demo.html', form=form, result=prediction_message, result_tone=prediction_message_tone,
+                               result_phone=prediction_message_phone)
     else:
-        return render_template('demo.html', form=form, result=None, result_tone=None)
+        return render_template('demo.html', form=form, result=None, result_tone=None, result_phone=None)
 
 
 @app.route('/about/')
@@ -85,5 +106,5 @@ def render_about():
 
 
 if __name__ == '__main__':
-    #app.run('127.0.0.1', 7799, debug=True)
+    # app.run('127.0.0.1', 7799, debug=True)
     app.run()  # for gunicorn server
